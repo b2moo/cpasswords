8eb2e3e21dcda0ef31f69e1eb77ceee0a4b8c307
Daniel STAN <daniel.stan@crans.org>
2012-05-24 10:40:40 +0200

    converter n'écrase plus

--- converter.py
+++ converter.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
 import json
 import glob
-import os
+import os,sys
 # Basic converter to json, avec roles nounous partout
 
 init_path = '/home/dstan/crans/passwords/'
@@ -12,7 +15,13 @@ filenames = glob.glob('*.asc')
 
 encoder=json.JSONEncoder()
 for filename in filenames:
-    nf = file(final_path+filename[:-4]+'.json','w')
+    fname=final_path+filename[:-4]+'.json'
+    if os.path.exists(fname):
+        print "%s already exists, ignored" % filename
+        continue
+    else:
+        print "Traitement de %s" % filename
+    nf = file(fname,'w')
     
     nf.write(encoder.encode({'roles':['nounous']\
         ,'contents':open(filename).read()}))
