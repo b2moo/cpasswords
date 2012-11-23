b74bb1ff1305ff8a3667a36bb21ec03b428feefa
Vincent Le Gallic <legallic@crans.org>
2012-11-23 04:27:23 +0100

    [config client] Le fichier du serveur s'appelle maintenant server
    Et non plus cranspasswords-server

--- clientconfig.example.py
+++ clientconfig.example.py
@@ -6,7 +6,7 @@ import os
 servers = {
     'default': {
         'server_cmd': ['/usr/bin/ssh', 'vert.adm.crans.org',\
-            '/root/cranspasswords/cranspasswords-server'],
+            '/root/cranspasswords/server'],
         'user' : os.getenv('USER')  # À définir à la main pour les personnes
                                   # n'ayant pas le même login sur leur pc
     }
