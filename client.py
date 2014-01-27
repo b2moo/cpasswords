8efeba0bec3273afa25d9b08943b52b12d14c6e7
Daniel STAN <daniel.stan@crans.org>
2014-01-27 22:40:41 +0100

    Get serverdata before parsing roles

--- client.py
+++ client.py
@@ -1021,9 +1021,9 @@ if __name__ == "__main__":
     # NB : ça nécessite de se connecter au serveur, or, pour show_servers on n'en a pas besoin
     # Il faudrait ptêtre faire ça plus proprement, en attendant, je ducktape.
     if options.action != show_servers:
-        options.roles = parse_roles(options)
         # On récupère les données du serveur à partir du nom fourni
         options.serverdata = get_server_data(options)
+        options.roles = parse_roles(options)
     
     # Si l'utilisateur a demandé une action qui nécessite un nom de fichier,
     # on vérifie qu'il a bien fourni un nom de fichier.
